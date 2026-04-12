66ea7dee1f9e94aefc9854270d6edba21dcddb4d
oblo0810 <oleboelling@pm.me>
2026-04-12 19:12:00 +0200

    Added benchmarks against other popular libraries.

--- benchmarks/test_python_module_bench.py
+++ benchmarks/test_python_module_bench.py
@@ -1,4 +1,6 @@
 import fastcube
+import magiccube
+import pycuber
 
 Cube2x2 = fastcube.Cube2x2
 
@@ -36,5 +38,35 @@ def test_benchmark_short_algorithm_on_same_cube(benchmark):
         cube.do_f_prime_move()
         cube.do_u_prime_move()
         cube.do_r_prime_move()
+
+    benchmark(run_alg)
+
+
+def test_benchmark_do_moves_20_turn_algorithm(benchmark):
+    cube = Cube2x2()
+    moves_20 = "R U R' U' F2 L D B' R2 U F' L2 D' B U2 R' F L' D2 B2"
+
+    def run_alg():
+        cube.do_moves(moves_20)
+
+    benchmark(run_alg)
+
+
+def test_benchmark_do_moves_20_turn_algorithm_magiccube(benchmark):
+    cube = magiccube.Cube(3)
+    moves_20 = "R U R' U' F2 L D B' R2 U F' L2 D' B U2 R' F L' D2 B2"
+
+    def run_alg():
+        cube.rotate(moves_20)
+
+    benchmark(run_alg)
+
+
+def test_benchmark_do_moves_20_turn_algorithm_pycuber(benchmark):
+    cube = pycuber.Cube()
+    moves_20 = "R U R' U' F2 L D B' R2 U F' L2 D' B U2 R' F L' D2 B2"
+
+    def run_alg():
+        cube(moves_20)
 
     benchmark(run_alg)
